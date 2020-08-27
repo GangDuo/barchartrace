87bc991e33cb4a59da71e02ff2e75765fa6930b1
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2020-08-27 17:07:04 +0900

    MySQL接続する

--- cron/index.js
+++ cron/index.js
@@ -1,1 +1,17 @@
-console.log('Hello')
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+})
+
+connection.connect()
+
+connection.query('SELECT "Hello World!" AS text', (error, results, fields) => {
+  if (error) throw error
+  console.log(results[0].text)
+})
+
+connection.end()
